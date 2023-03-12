548a33d91d57c37906cc92be47ee6c1194446f9c
juliannea <112399053+juliannea@users.noreply.github.com>
2023-03-12 17:25:30 -0400

    Add files via upload

---
+++ decrypt.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "caesar.h"
+#include "vigenere.h"
+
+
+
+std::string decryptCaesar(std::string ciphertext, int rshift)
+{
+    int lshift = 26 - rshift;
+    return encryptCaesar(ciphertext, lshift);
+}
+
+std::string decryptVigenere(std:: string ciphertext, std::string keyword)
+{
+     std::string key;
+
+    for (int i = 0; i < keyword.length(); i++) 
+    {
+        int ogkey = keyshift(keyword[i]); //finds the int value of letter 
+        if (ogkey == 0)  
+        {
+            key += keyword[i]; 
+        } 
+        else 
+        {
+            int num = 26 - ogkey;
+            key += (char)(97 + num);
+        }
+    }
+    return encryptVigenere(ciphertext, key);
+}
